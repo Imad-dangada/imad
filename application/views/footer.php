d9ebaed845afab5e547967f76e82be2fc898e110
Your Name <imadmato@gmail.com>
2021-06-26 11:28:06 -0700

    my resume created using codeigniter

---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <a href="https://facebook.com/ibrahimmato.dangada"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+  <a href="https://instagra.com/imaddangada"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+  <a href="https://twitter.com/imaddangada"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  
+</footer>
+
+</body>
+</html>
